2fc05aebaf4e5812376a23c9d960dcc201789be6
linpengteng <china.linpengteng@gmail.com>
2022-10-01 17:49:14 +0800

    init: Vue2.x Sticker Table

---
+++ dist/vue-loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    v-if="loading"
+    class="s-spin-nested-spining"
+  >
+    <span class="s-spin-dot-group">
+      <i class="s-spin-dot-item" />
+      <i class="s-spin-dot-item" />
+      <i class="s-spin-dot-item" />
+      <i class="s-spin-dot-item" />
+    </span>
+
+    <div class="s-spin-text">
+      <span>正在加载中...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StickerLoading',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "./vue-loading.less";
+</style>
